feafb9dc06d192d89b52db962e6a446ee2525283
jupabass89 <jupabass89@gmail.com>
2019-06-27 15:16:13 -0500

    Getting-data-from-view: some atributes to some tags were added as well as some code was added to comunicate de view with the component.ts file

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,7 +13,9 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- wolox-books/src/app/login/login.component.ts
+++ wolox-books/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  password: string = "";
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private fb: FormBuilder) { 
+    this.form = fb.group({
+      'email': [null, Validators.required],
+      'password': [null, Validators.required],
+      'password_confirmation': this.password, 
+      'first_name': [null, Validators.required],
+      'last_name': [null, Validators.required],
+      'locale': "en"
+    })
   }
 
+  ngOnInit() {
+
+  }
+
+  login() {
+    console.log(this.form.value);
+  }
 }
