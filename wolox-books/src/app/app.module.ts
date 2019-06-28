53eb55fca628397202e4b11ef24246241e379c10
jupabass89 <jupabass89@gmail.com>
2019-06-28 17:11:12 -0500

    the register component was added

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
---
+++ wolox-books/src/app/register/register.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+
+  form: FormGroup;
+  post: any;
+
+  constructor(private fb: FormBuilder) { 
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      'email': [null, Validators.required],
+      'password': [null, Validators.required], 
+      'first_name': [null, Validators.required],
+      'last_name': [null, Validators.required]
+    })
+  }
+
+  singUp(post) {
+    var email = post.email;
+    var password = post.password;
+    var password_confirmation = post.password;
+    var first_name = post.first_name;
+    var last_name = post.last_name;
+    var locale = "en";
+    console.log(JSON.stringify({user:{email , password , password_confirmation, first_name, last_name, locale}})); 
+  }
+
+}
