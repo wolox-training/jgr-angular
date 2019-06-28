c91fbb1173935c9849a6250b3fb0748a46f6d8cc
jupabass89 <jupabass89@gmail.com>
2019-06-28 17:51:23 -0500

    refactoring:the entire project was refactored acording with the las PR advices

--- wolox-books/src/app/register/register.component.ts
+++ wolox-books/src/app/register/register.component.ts
@@ -7,30 +7,27 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
   form: FormGroup;
-  post: any;
 
-  constructor(private fb: FormBuilder) { 
-  }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.form = this.fb.group({
       'email': [null, Validators.required],
       'password': [null, Validators.required], 
-      'first_name': [null, Validators.required],
-      'last_name': [null, Validators.required]
+      'firstName': [null, Validators.required],
+      'lastName': [null, Validators.required]
     })
   }
 
-  singUp(post) {
-    var email = post.email;
-    var password = post.password;
-    var password_confirmation = post.password;
-    var first_name = post.first_name;
-    var last_name = post.last_name;
-    var locale = "en";
-    console.log(JSON.stringify({user:{email , password , password_confirmation, first_name, last_name, locale}})); 
+  singUp(post: any) {
+    let email = post.email;
+    let password = post.password;
+    let password_confirmation = post.password;
+    let first_name = post.firstName;
+    let last_name = post.lastName;
+    let locale = "en";
+    console.log(JSON.stringify({user:{ email , password , password_confirmation, first_name, last_name, locale}})); 
   }
 
 }
