fb766265ce62bb1d512c1b73b45f69bff28804d9
jupabass89 <jupabass89@gmail.com>
2019-06-28 11:58:04 -0500

    jason-print: the method singUp with the respective code for printing in the console was added

--- wolox-books/src/app/login/login.component.ts
+++ wolox-books/src/app/login/login.component.ts
@@ -7,25 +7,28 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  password: string = "";
   form: FormGroup;
+  post: any;
 
   constructor(private fb: FormBuilder) { 
-    this.form = fb.group({
-      'email': [null, Validators.required],
-      'password': [null, Validators.required],
-      'password_confirmation': this.password, 
-      'first_name': [null, Validators.required],
-      'last_name': [null, Validators.required],
-      'locale': "en"
-    })
   }
 
   ngOnInit() {
-
+    this.form = this.fb.group({
+      'email': [null, Validators.required],
+      'password': [null, Validators.required], 
+      'first_name': [null, Validators.required],
+      'last_name': [null, Validators.required]
+    })
   }
 
-  login() {
-    console.log(this.form.value);
+  singUp(post) {
+    var email = post.email;
+    var password = post.password;
+    var password_confirmation = post.password;
+    var first_name = post.first_name;
+    var last_name = post.last_name;
+    var locale = "en";
+    console.log(JSON.stringify({user:{email , password , password_confirmation, first_name, last_name, locale}}));
   }
 }
