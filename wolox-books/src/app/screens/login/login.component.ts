8c6b018738cdd07c1ae7d74dc8448d8db416010b
jupabass89 <jupabass89@gmail.com>
2019-07-04 12:08:55 -0500

    login-set-up:the login component and the integration with the web service were setted

--- wolox-books/src/app/screens/login/login.component.ts
+++ wolox-books/src/app/screens/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['../register/register.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  
+  constructor(private fb: FormBuilder, private service: UserService, private router: Router) { }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      'email': [null, Validators.required],
+      'password': [null, Validators.required]
+    })
   }
 
+  login(post: any) {
+    let session = {
+      email: post.email,
+      password: post.password,
+    };
+
+    console.log({session}); 
+    this.service.createSession({session}).subscribe(
+    response => {console.log('access_token:' + response.access_token)})
+  }
 }
