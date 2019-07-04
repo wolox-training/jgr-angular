4c39087a10f5e1a9750a45dc519c28bb426041dd
jupabass89 <jupabass89@gmail.com>
2019-07-04 11:08:04 -0500

    improving the code: some code was refactored to improve the functionality

--- wolox-books/src/app/screens/register/register.component.ts
+++ wolox-books/src/app/screens/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { UserService } from '../../services/user.service' ;
+import { UserService } from '../../services/user.service';
 
 
 @Component({
@@ -10,33 +10,29 @@ import { UserService } from '../../services/user.service' ;
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
-  local: string = "en";
 
-  constructor(private fb: FormBuilder, private service: UserService) {}
+  constructor(private fb: FormBuilder, private service: UserService) { }
 
   ngOnInit() {
     this.form = this.fb.group({
       'email': [null, Validators.required],
-      'password': [null, Validators.required], 
+      'password': [null, Validators.required],
       'firstName': [null, Validators.required],
       'lastName': [null, Validators.required]
     })
   }
 
   singUp(post: any) {
-    let user = { 
-      email: post.email, 
-      password: post.password, 
-      password_confirmation: post.password, 
-      first_name: post.firstName, 
+    let user = {
+      email: post.email,
+      password: post.password,
+      password_confirmation: post.password,
+      first_name: post.firstName,
       last_name: post.lastName,
       locale: "en"
     };
 
-    console.log({user});
-    this.service.createUser(user).subscribe(
-      result => {if (result.status == 201) console.log('succsess')} 
-      ,error => {if (error.status == 422) console.log('non-succsess')}
-    );
+    console.log({ user });
+    this.service.createUser({user}).subscribe(() => {console.log('succsess')});
   }
 }
