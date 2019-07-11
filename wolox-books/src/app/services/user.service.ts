79c1cc7edcc5a7f2f779847a0e6d8dbb16400cba
jupabass89 <jupabass89@gmail.com>
2019-07-11 15:35:05 -0500

    some code refactoring was made due to the trainer advices of the current PR

--- wolox-books/src/app/services/user.service.ts
+++ wolox-books/src/app/services/user.service.ts
@@ -4,7 +4,6 @@ import { Observable } from 'rxjs';
 import { LocalStorageService } from './local-storage.service';
 import { environment } from '../../environments/environment';
 
-
 @Injectable({
   providedIn: 'root'
 })
