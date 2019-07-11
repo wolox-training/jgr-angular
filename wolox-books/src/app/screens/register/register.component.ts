6b0e73c97fed47996f9cadc5ec11bc203a8db42e
jupabass89 <jupabass89@gmail.com>
2019-07-11 16:23:20 -0500

    some code refactoring was made

--- wolox-books/src/app/screens/register/register.component.ts
+++ wolox-books/src/app/screens/register/register.component.ts
@@ -17,25 +17,27 @@ export class RegisterComponent implements OnInit {
     private service: UserService,
     private router: Router
   ) {}
-  
   ngOnInit() {
     this.form = this.fb.group({
-      'email': [null, Validators.required],
-      'password': [null, Validators.required],
-      'firstName': [null, Validators.required],
-      'lastName': [null, Validators.required]
-    })
+      email: [null, Validators.required],
+      password: [null, Validators.required],
+      firstName: [null, Validators.required],
+      lastName: [null, Validators.required]
+    });
   }
 
   singUp(post: any) {
-    let user = {
+    const user = {
       email: post.email,
       password: post.password,
       password_confirmation: post.password,
       first_name: post.firstName,
       last_name: post.lastName,
-      locale: "en"
+      locale: 'en'
     };
-    this.service.createUser({user}).subscribe(() => this.router.navigate(['login']));
+    this.service.createUser({user}).subscribe(() => {
+      this.router.navigate(['login']);
+      console.log('success');
+    });
   }
 }
