e6c1ab8c6b17cf7adef6700d60f44f7f5741ad11
jupabass89 <jupabass89@gmail.com>
2019-07-15 14:12:21 -0500

    resolving some conflicts

--- wolox-books/src/app/screens/auth/auth.component.ts
+++ wolox-books/src/app/screens/auth/auth.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { Router } from '@angular/router';
-import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent{
- 
-  constructor(private router: Router, private store: LocalStorageService) {}
+export class AuthComponent {
+  constructor(
+    private router: Router,
+    private store: LocalStorageService
+  ) {}
 
   logout() {
     this.store.clearStorage();
