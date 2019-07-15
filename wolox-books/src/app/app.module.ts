bfdad4bb292620de10698510c184ee0dadef6f07
jupabass89 <jupabass89@gmail.com>
2019-07-15 13:50:39 -0500

    resolving some merge conflicts

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -7,8 +7,7 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './screens/register/register.component';
 import { HttpClientModule } from '@angular/common/http';
 import { LoginComponent } from './screens/login/login.component';
-import { AuthComponent } from './screens/auth/auth.component';  
-
+import { AuthComponent } from './screens/auth/auth.component';
 @NgModule({
   declarations: [
     AppComponent,
